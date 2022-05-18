001a9ab52a0c302d0d7ede8e43150050ca9a5c8e
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-05-18 13:52:49 +0200

    Add project files.

---
+++ VSforWEB ex 4/Kalender.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VSforWEB_ex_4
+{
+    public partial class Kalender : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                Datum.Text = Convert.ToString(Calendar1.SelectedDate.ToLongDateString());
+            }
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
